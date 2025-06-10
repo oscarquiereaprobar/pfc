a02f4781fa0deeebc53a67fa7278187377da20d9
ocar <aw.oscargarcia2@gmail.com>
2025-06-10 23:01:20 +0200

    porfi

--- backend/src/main/java/com/example/demo/config/MvcConfig.java
+++ backend/src/main/java/com/example/demo/config/MvcConfig.java
@@ -2,21 +2,29 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                    "https://pfc-m3eu5l08g-oscarquiereaprobars-projects.vercel.app",
-                    "https://pfc-sigma.vercel.app",
-                    "https://pfc-git-main-oscarquiereaprobars-projects.vercel.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins(
+	                    "https://pfc-m3eu5l08g-oscarquiereaprobars-projects.vercel.app",
+	                    "https://pfc-sigma.vercel.app",
+	                    "https://pfc-git-main-oscarquiereaprobars-projects.vercel.app"
+	            )
+	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	            .allowedHeaders("*")
+	            .allowCredentials(true);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/uploads/**")
+	            .addResourceLocations("file:/home/site/wwwroot/uploads/");
+	}
 }
+
